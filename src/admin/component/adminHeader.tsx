66b42c0f0ab556f72d2a955f99ad692bbaf6c36c
Okeke Chiemerie <charleschiemerie2004@gmail.com>
2026-01-30 13:43:19 +0100

    Removed some unnecessary imports

--- src/admin/component/adminHeader.tsx
+++ src/admin/component/adminHeader.tsx
@@ -1,18 +1,18 @@
-import { User2 } from "lucide-react";
-import { useSidebar } from "../../../context/sideBarContext";
-import Logo from "../../../components/logo";
-const Header = () => {
-    const { sideBar } = useSidebar();
-    return (
-        <header className={`flex justify-between items-center px-10 w-full h-15`}>
-            <div>
-                <Logo />
-            </div>
-            <div className="p-2 rounded-full bg-gray-100 ">
-                <User2 />
-            </div>
-        </header>
-    );
-}
+// import { User2 } from "lucide-react";
+// import { useSidebar } from "../../../context/sideBarContext";
+// import Logo from "../../../components/logo";
+// const Header = () => {
+//     const { sideBar } = useSidebar();
+//     return (
+//         <header className={`flex justify-between items-center px-10 w-full h-15`}>
+//             <div>
+//                 <Logo />
+//             </div>
+//             <div className="p-2 rounded-full bg-gray-100 ">
+//                 <User2 />
+//             </div>
+//         </header>
+//     );
+// }
 
-export default Header;
+// export default Header;
--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import SidebarProvider from './context/sideBarContext.tsx'
-import Sidebar from './components/sidebar.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
