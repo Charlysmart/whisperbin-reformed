0c6b0cee3c6f859707c0c782f3c99dff278fbe5c
Okeke Chiemerie <charleschiemerie2004@gmail.com>
2026-02-25 16:27:50 +0100

    Added lazy to the router for optimization

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,59 @@
-import Inbox from "@/user/pages/inbox";
-import Login from "@/user/pages/login";
-import Chat from "@/user/pages/chat";
-import UserProfile from "@/user/pages/userProfile";
-import StatusFeed from "@/user/pages/statusFeed";
-import SendMessage from "@/user/pages/sendMessage";
+import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Register from "@/user/pages/register";
+const Inbox = lazy(() => import("@/user/pages/inbox"));
+const Login = lazy(() => import("@/user/pages/login"));
+const Chat = lazy(() => import("@/user/pages/chat"));
+const UserProfile = lazy(() => import("@/user/pages/userProfile"));
+const SendMessage = lazy(() => import("@/user/pages/sendMessage"));
+const Register = lazy(() => import("@/user/pages/register"));
+const Verify = lazy(() => import("@/user/pages/verify"));
+const Notification = lazy(() => import("@/user/pages/notification"));
+const AnonymousChat = lazy(() => import("@/user/pages/anonymous"));
+const CreateRoom = lazy(() => import("@/user/pages/create_room"));
+const JoinRoom = lazy(() => import("@/user/pages/join_room"));
+const Whisperroom = lazy(() => import("@/user/pages/whisperroom"));
+const NotFound = lazy(() => import("@/components/not_found"));
+const AdminDashboard = lazy(() => import("@/admin/dashboard"));
+const Home = lazy(() => import("@/home"));
 import SidebarLayout from "@/user/layout/sidebarLayout";
-import Verify from "@/user/pages/verify";
-import ViewStatus from "@/user/pages/viewStatus";
-import Notification from "@/user/pages/notification";
 import { GradientProvider } from "@/user/components/gradient";
 import PreloaderProvider from "@/context/loaderContext";
-import AnonymousChat from "@/user/pages/anonymous";
-import CreateRoom from "@/user/pages/create_room";
-import JoinRoom from "@/user/pages/join_room";
-import Whisperroom from "@/user/pages/whisperroom";
-import NotFound from "@/components/not_found";
-import AdminDashboard from "./admin/dashboard";
 import ServerCheck from "./components/serverGate";
 import UserAuthentication from "./protected_routes/user_auth";
-import Home from "./home";
+import Preloader from "./components/preloader";
 
 
 function App() {
   return (
     <Router>
-      <GradientProvider />
-      <Routes>
-        <Route element={<PreloaderProvider />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/verify" element={<Verify />} />
-          <Route path="*" element={<NotFound />} />
-          <Route element={<ServerCheck />} >
-            <Route element={<UserAuthentication />}>
-              <Route element={<SidebarLayout />}>
-                <Route path="/user_profile" element={<UserProfile />} />
-                <Route path="/inbox" element={<Inbox />} />
-                <Route path="/send_message/:username?" element={<SendMessage />} />    
-                <Route path="/anonymous_messages" element={<AnonymousChat />} />
-                <Route path="/chat/:thread" element={<Chat />} />        
-                <Route path="/notifications" element={<Notification />} /> 
-                <Route path="/create_room"  element={<CreateRoom />} />
-                <Route path="/join_room"  element={<JoinRoom />} />
-                <Route path="whisperroom/:room_thread" element={<Whisperroom />} />
+      <Suspense fallback={<Preloader />} >
+        <GradientProvider />
+        <Routes>
+          <Route element={<PreloaderProvider />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/verify" element={<Verify />} />
+            <Route path="*" element={<NotFound />} />
+            <Route element={<ServerCheck />} >
+              <Route element={<UserAuthentication />}>
+                <Route element={<SidebarLayout />}>
+                  <Route path="/user_profile" element={<UserProfile />} />
+                  <Route path="/inbox" element={<Inbox />} />
+                  <Route path="/send_message/:username?" element={<SendMessage />} />    
+                  <Route path="/anonymous_messages" element={<AnonymousChat />} />
+                  <Route path="/chat/:thread" element={<Chat />} />        
+                  <Route path="/notifications" element={<Notification />} /> 
+                  <Route path="/create_room"  element={<CreateRoom />} />
+                  <Route path="/join_room"  element={<JoinRoom />} />
+                  <Route path="whisperroom/:room_thread" element={<Whisperroom />} />
+                </Route>
               </Route>
             </Route>
+            <Route path="/dashboard" element={<AdminDashboard />} />
           </Route>
-          <Route path="/dashboard" element={<AdminDashboard />} />
-        </Route>
-      </Routes>
+        </Routes>
+      </Suspense>
     </Router>
   );
 }
